d61b0410c9dc219d78ef0620f63012a5947b7114
leoppro <i@leop.pro>
2023-06-03 23:06:18 +0800

    update fetch tags script

--- fetch_tags_need_push.py
+++ fetch_tags_need_push.py
@@ -3,10 +3,11 @@
 
 import re
 import requests
+from packaging import version
 
 
-def fetch_tags(filter=None):
-    url = 'https://hub.docker.com/v2/repositories/library/caddy/tags/?ordering=-last_updated&page_size=100&page=1'
+def fetch_tags(repo, filter=None):
+    url = 'https://hub.docker.com/v2/repositories/%s/tags/?ordering=-last_updated&page_size=100&page=1' % repo
     tags = []
     while url != None:
         req = requests.get(url)
@@ -18,13 +19,23 @@ def fetch_tags(filter=None):
                 continue
             if filter(result['name']):
                 tags.append(result['name'])
-    return tags
+    return sorted(tags, key=lambda x: version.Version(x))
 
 
 def main():
-    tags = fetch_tags(lambda tag: re.match(r'^\d+\.\d+\.\d+$', tag))
-    for tag in tags:
-        print(tag, end=',')
+    this_repo_tags = fetch_tags(
+        'leoppro/docker-caddy-webdav', lambda tag: re.match(r'^\d+\.\d+\.\d+$', tag))
+    first_this_repo_tags = '0.0.0'
+    if len(this_repo_tags) > 0:
+        first_this_repo_tags = this_repo_tags[0]
+    caddy_tags = fetch_tags(
+        'library/caddy', lambda tag: re.match(r'^\d+\.\d+\.\d+$', tag) and version.Version(tag) >= version.Version(first_this_repo_tags))
+    need_push_tags = list(set(caddy_tags).difference(set(this_repo_tags)))
+    need_push_tags = sorted(need_push_tags, key=lambda x: version.Version(x))
+    if len(need_push_tags) == 0:
+        print('push_tag=')
+        return
+    print('push_tag=%s' % need_push_tags[0])
 
 
 if __name__ == '__main__':
